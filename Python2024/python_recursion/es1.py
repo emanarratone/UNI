6c6a15f45afa65cce87e894e2ad7bc893a7e3229
emanarratone <160839713+emanarratone@users.noreply.github.com>
2025-09-27 00:51:40 +0200

    Rename function to 'fibonacci' and update calls

--- Python2024/python_recursion/es1.py
+++ Python2024/python_recursion/es1.py
@@ -1,10 +1,11 @@
-def ennesimo_fibonacci(n):
+def fibonacci(n):
     if n == 0:
         return 0
     elif n == 1:
         return 1
     else:
-        return ennesimo_fibonacci(n-1) + ennesimo_fibonacci(n-2)
+        return fibonacci(n-1) + fibonacci(n-2)
 
 
 print(ennesimo_fibonacci(10))
+
